6cd2f721df75c9165c3024c1d1f107aa65e403a5
Craig Fleming <rythos42@gmail.com>
2019-11-03 15:42:55 -0800

    Refactored to remove auth-utils.js

--- src/ladder/src/storage/models/auth.js
+++ src/ladder/src/storage/models/auth.js
@@ -1,19 +1,37 @@
-import {
-  msalApp,
-  requiresInteraction,
-  fetchMsGraph,
-  GRAPH_ENDPOINTS,
-  GRAPH_REQUESTS
-} from "./auth-utils";
+import { UserAgentApplication } from "msal";
+
+const GRAPH_SCOPES = {
+  OPENID: "openid",
+  PROFILE: "profile",
+  USER_READ: "User.Read",
+  MAIL_READ: "Mail.Read"
+};
+
+const GRAPH_REQUESTS = {
+  LOGIN: {
+    scopes: [GRAPH_SCOPES.OPENID, GRAPH_SCOPES.PROFILE, GRAPH_SCOPES.USER_READ]
+  }
+};
+
+const GRAPH_ENDPOINTS = {
+  ME: "https://graph.microsoft.com/v1.0/me"
+};
 
 export default {
   state: {
+    msalApp: null,
     error: "",
     account: null,
     graphProfile: {},
     showDebug: false
   },
   reducers: {
+    setMsalApp(state, msalApp) {
+      return {
+        ...state,
+        msalApp
+      };
+    },
     setAuthError(state, error) {
       return {
         ...state,
@@ -41,10 +59,16 @@ export default {
   },
   effects: dispatch => ({
     async acquireToken(request, state) {
-      return msalApp.acquireTokenSilent(request).catch(error => {
+      return state.auth.msalApp.acquireTokenSilent(request).catch(error => {
         // Call acquireTokenPopup (popup window) in case of acquireTokenSilent failure due to consent or interaction required ONLY
-        if (requiresInteraction(error.errorCode))
-          msalApp.acquireTokenPopup(request);
+        if (!error.errorCode || !error.errorCode.length) return;
+
+        const requiresInteraction =
+          error.errorCode.indexOf("consent_required") > -1 ||
+          error.errorCode.indexOf("interaction_required") > -1 ||
+          error.errorCode.indexOf("login_required") > -1;
+
+        if (requiresInteraction) state.auth.msalApp.acquireTokenPopup(request);
       });
     },
 
@@ -56,18 +80,36 @@ export default {
         });
 
       if (tokenResponse) {
-        const graphProfile = await fetchMsGraph(
-          GRAPH_ENDPOINTS.ME,
-          tokenResponse.accessToken
-        ).catch(() => {
-          dispatch.auth.setAuthError("Unable to fetch Graph profile.");
+        const response = await fetch(GRAPH_ENDPOINTS.ME, {
+          headers: {
+            Authorization: `Bearer ${tokenResponse.accessToken}`
+          }
         });
 
+        const graphProfile = response.json();
         if (graphProfile) dispatch.auth.setGraphProfile(graphProfile);
       }
     },
 
     async initialize(_, state) {
+      const msalApp = new UserAgentApplication({
+        auth: {
+          clientId: "7a65bab1-4cf7-432d-b878-4f0c6ae52aea",
+          authority:
+            "https://login.microsoftonline.com/95d3bdcc-5ffa-4580-a8e2-241b051d339a",
+          validateAuthority: true,
+          postLogoutRedirectUri: "http://localhost:3000",
+          navigateToLoginRequestUrl: false
+        },
+        cache: {
+          cacheLocation: "sessionStorage"
+        },
+        system: {
+          navigateFrameWait: 0
+        }
+      });
+      dispatch.auth.setMsalApp(msalApp);
+
       const account = msalApp.getAccount();
       dispatch.auth.setAccount(account);
 
@@ -75,7 +117,7 @@ export default {
     },
 
     async requestSignIn(_, state) {
-      const loginResponse = await msalApp
+      const loginResponse = await state.auth.msalApp
         .loginPopup(GRAPH_REQUESTS.LOGIN)
         .catch(error => {
           dispatch.auth.setAuthError(error.message);
@@ -88,7 +130,7 @@ export default {
     },
 
     async requestSignOut(_, state) {
-      msalApp.logout();
+      state.auth.msalApp.logout();
     },
 
     toggleShowDebug(_, state) {
